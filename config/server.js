273dbda54de57d41b3dccb5f8457c65be2794595
Yanuar Aditia <yanuaraditia@outlook.com>
2020-09-14 16:36:41 +0700

    ST

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4d8796bed91dd799db771beea085c83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a8cc3d37-0278-4778-ad20-9dd410b240f5'
+};
